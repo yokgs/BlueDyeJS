6614325317ace250f3a0e70456a0c1c1991772b6
Yazid Slila <70753201+yokgs@users.noreply.github.com>
2021-07-17 18:11:03 +0100

    Update bluedye.js

--- src/bluedye.js
+++ src/bluedye.js
@@ -1,1 +1,62 @@
-// hello blue
+/**
+ * BlueDyeJS v1.0.0
+ * by Yazid SLILA (@yokgs)
+ * under MIT License
+ */
+"use strict";
+(function () {
+    var rgb = (r, g, b) => [r, g, b];
+    var bluedye = function (color) {
+        return new bluedye.y.color(color);
+    };
+    bluedye.y = bluedye.prototype = {
+        color: function (color) {
+            //default values
+            var s = [0, 0, 0];
+
+            if (typeof color == "string") {
+                try {
+                    s = eval(color);
+                } catch (_) { };
+            }
+
+            this.RED = s[0];
+            this.GREEN = s[1];
+            this.BLUE = s[2];
+            return this;
+        },
+        red: function (red) {
+            this.RED = red;
+            return this;
+        },
+        green: function (green) {
+            this.GREEN = green;
+            return this;
+        },
+        blue: function (blue) {
+            this.BLUE = blue;
+            return this;
+        },
+        negative: function () {
+            this.RED = 255 - this.RED;
+            this.GREEN = 255 - this.GREEN;
+            this.BLUE = 255 - this.BLUE;
+            return this;
+        },
+        redToBlue: function () {
+            let t = this.RED;
+            this.RED = this.GREEN;
+            thi.GREEN = this.BLUE;
+            this.BLUE = t;
+            return this;
+        },
+        blueToRed: function () {
+            let t = this.BLUE;
+            this.BLUE = this.GREEN;
+            thi.GREEN = this.RED;
+            this.RED = t;
+            return this;
+        },
+    }
+    window.bluedye = bluedye;
+}())
